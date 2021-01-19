f5a56bdb70aa1bfbcdd5e508486685c6afe2cfa6
Murphy.W.Zhu <Murphy.W.Zhu@aliyun.com>
2021-01-19 18:51:34 +0800

    change fcitx version to 5 and change wallpaper directory to userhome

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,13 +2,12 @@
 
 xinput disable "ETPS/2 Elantech Touchpad"
 picom -b --backend glx
-feh --randomize --bg-fill /usr/share/murch-wallpaper
+feh --randomize --bg-fill ~/Pictures/wallpapers/
 xss-lock ~/.config/muscript/muxss-lock.sh &
 while true; do
-        #xsetroot -name "$(pamixer --get-volume-human) ⌚$( date +"%R" )"
         ~/.config/muscript/mudwmbar.sh
     sleep 2s
 done &
 ~/.config/muscript/cpuused.sh &
 nm-applet &
-fcitx &
+fcitx5 &
